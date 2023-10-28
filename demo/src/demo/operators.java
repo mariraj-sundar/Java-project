05c416a014e2540b404aeb549fba40b5bac1565e
ELCOT <ELCOT@Lenovo>
2023-10-28 14:12:46 +0530

    commit second

---
+++ demo/src/demo/operators.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class operators {
+  public static void main(String[] args) {
+	  int a=4,b=3;
+	  System.out.println("add of a and b: "+(a+b));
+	  System.out.println("sub of a and b: "+(a-b));
+	  System.out.println("mutiply of a and b: "+(a*b));
+	  System.out.println("divison of a and b: "+(a/b));
+	  System.out.println(a&b);
+	  System.out.println(a|b);
+	  System.out.println(a^b);
+	  int x=7,y=4;
+	  System.out.println(a==b);
+	  System.out.println(!(a!=b));
+	  System.out.println(a!=b);
+  }
+}
